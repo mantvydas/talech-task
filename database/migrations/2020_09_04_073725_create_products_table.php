2b1c66c6c674288037c7611c6cd83e34c87ccea9
Mantvydas <mantvydas.sarkus@gmail.com>
2020-09-04 11:54:50 +0300

    Created product models and migrations

---
+++ database/migrations/2020_09_04_073725_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('ean')->unique();
+            $table->enum('type', ['mens', 'ladies']);
+            $table->double('weight', 8, 2);
+            $table->string('color');
+            $table->boolean('active');
+            $table->double('price', 8, 2);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
