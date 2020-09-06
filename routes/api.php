7c67b9804e6f26c29fca66b07d6c5d978f6811bc
Mantvydas <mantvydas.sarkus@gmail.com>
2020-09-06 23:22:06 +0300

    Created product CRUD for API using sanctum

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/sanctum/token', 'Auth\LoginController@loginUsingSanctum');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')->apiResource('products', 'Api\ProductController');
