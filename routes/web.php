25e6bc7aea7af58d5a37ca08ac131b3d569b636b
Mantvydas <mantvydas.sarkus@gmail.com>
2020-09-06 21:23:50 +0300

    Added multi language support

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('language/{locale}', 'ChangeLocale');
+
 Auth::routes();
 Route::get('/products/trash', 'ProductController@trash')
     ->middleware('auth')
